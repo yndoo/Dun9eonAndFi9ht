19524963052d1581be13b7409e9a9648a5b128c6
Jaewon <dhwodnjs0827@naver.com>
2025-02-07 11:01:49 +0900

    [ Chore ] 몬스터 생성자 추가
    // Character 클래스 상속
    
    // Reward 구조체 추가
    
    // 몬스터 생성자 추가

--- Dun9eonAndFi9ht/Characters/Monster.cs
+++ Dun9eonAndFi9ht/Characters/Monster.cs
@@ -6,12 +6,42 @@ using System.Threading.Tasks;
 
 namespace Dun9eonAndFi9ht.Characters
 {
-    internal class Monster
+    public struct Reward
     {
-        struct Reward
+        public int exp;
+        public int gold;
+
+        public Reward(int exp, int gold)
         {
-            public int exp;
-            public int gold;
+            this.exp = exp;
+            this.gold = gold;
+        }
+    }
+
+    internal class Monster : Character
+    {
+        private Reward reward;
+
+        public Reward Reward => reward;
+
+        public Monster(string name, int maxHp, int atk, int def, int level, int exp, int gold) : base(name, maxHp, atk, def, level)
+        {
+            reward = new Reward(exp, gold);
+        }
+
+        public override void Attack(Character target)
+        {
+            base.Attack(target);
+        }
+
+        public override void Damaged(int damage)
+        {
+            base.Damaged(damage);
+        }
+
+        public override void Dead()
+        {
+            base.Dead();
         }
     }
 }
