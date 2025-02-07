cf32f3bbe75db88f19e11b21a1ff1a487fff7bc1
N0name4-SDH <soh0927@naver.com>
2025-02-07 17:27:11 +0900

    [ Feat ] GameManager가 player 받는 부분, 시작 함수 수정
    1. GameManager의 GetPlayerInfo 메서드를 만들어 GameManager의 player가 값을 받을 수 있도록 만들었습니다.
    2. Program.cs에서 플레이어 정보를 DB로부터 받고 게임을 시작하도록 로직을 수정하였습니다.

--- Dun9eonAndFi9ht/Manager/GameManager.cs
+++ Dun9eonAndFi9ht/Manager/GameManager.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataDefinition;
 using Dun9eonAndFi9ht.Characters;
 using Dun9eonAndFi9ht.Scenes;
+using Dun9eonAndFi9ht.StaticClass;
 
 namespace Dun9eonAndFi9ht.Manager
 {
@@ -12,7 +14,7 @@ namespace Dun9eonAndFi9ht.Manager
     {
         
         private static GameManager? instance;
-        public static GameManager Instance => instance ?? = new GameManager();
+        public static GameManager Instance => instance ??= new GameManager();
 
         private Player player;
         private List<Scene> sceneList;
@@ -21,7 +23,6 @@ namespace Dun9eonAndFi9ht.Manager
 
         private GameManager()
         {
-            player = new Player();
             sceneList = new List<Scene>();
             sceneList.Add(new StartScene());
         }
@@ -72,6 +73,21 @@ namespace Dun9eonAndFi9ht.Manager
             {
                 Console.WriteLine("잘못된 씬입니다.");
             }
+        }
+
+        /// <summary>
+        /// GameManager의 Player 정보를 줘 새 플레이어 인스턴스를 만드는 메서드
+        /// </summary>
+        /// <param name="name">이름</param>
+        /// <param name="job">직업</param>
+        /// <param name="maxHp">최대 Hp</param>
+        /// <param name="atk">공격력</param>
+        /// <param name="def">방어력</param>
+        /// <param name="level">레벨</param>
+        /// <param name="gold">골드</param>
+        public void GetPlayerInfo(string name, DataDefinition.EJobType job, int maxHp, int atk, int def, int level, int gold)
+        {
+            this.Player = new Player(name, job, maxHp, atk, def, level, gold);
         }
     }
 }
