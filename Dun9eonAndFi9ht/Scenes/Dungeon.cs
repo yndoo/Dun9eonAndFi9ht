6fa4e98fc4282311a10b51282f51507887bb067a
yndoo <isabelle2k@naver.com>
2025-02-07 11:06:08 +0900

    [ Feat ] Dungeon 시작 함수, 던전 입장 함수 작성
    // 추후 기능이 생긴 후에 수정 필요함

--- Dun9eonAndFi9ht/Scenes/Dungeon.cs
+++ Dun9eonAndFi9ht/Scenes/Dungeon.cs
@@ -1,4 +1,6 @@
 ﻿using Dun9eonAndFi9ht.Characters;
+using Dun9eonAndFi9ht.Manager;
+using Dun9eonAndFi9ht.StaticClass;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,9 +17,36 @@ namespace Dun9eonAndFi9ht.Scenes
         public Player Player { get; set; }
         public List<Monster> MonsterList { get; set; }
         public bool IsPlayerWin { get; set; }
+
+        /// <summary>
+        /// 몬스터 랜덤 배치, 플레이어 데이터 가져오기
+        /// </summary>
         private void EnterDungeon()
         {
+            //player = GameManager.Instance.Player;
 
+            //Monster 데이터 가져오기 예시
+            // To Do : 기능 생긴 뒤에 수정해야 함
+            //for(int i = 0; i < MonsterCount; i++)
+            //{
+            //    monsterList.Add(new Monster());
+            //    monsterList[i].Name = GameManager.GetMonsterData[i][0];
+            //    monsterList[i].MaxHp = GameManager.GetMonsterData[i][1];
+            //}
+        }
+
+        /// <summary>
+        /// Dungeon 시작 함수
+        /// </summary>
+        public override void Start()
+        {
+            base.Start();
+            Console.WriteLine("Battle!!");
+            EnterDungeon();
+
+            Random random = new Random();
+            // To Do : 기능 생긴 뒤에 수정해야 함
+            //BattleSystem.BattleTurn(Player, MonsterList.OrderBy(x => random.Next(0, 3)).ToList());
         }
     }
 }
