89187634edbc0bc79969ada4df689e5350bacf9b
N0name4-SDH <soh0927@naver.com>
2025-02-07 11:15:30 +0900

    [ Feat ] PrintMenu 부분 조정
    1. (0,15) 아래 부분에서 출력, 콘솔 화면 비우기 기능이 동작하도록 변경

--- Dun9eonAndFi9ht/StaticClass/Utility.cs
+++ Dun9eonAndFi9ht/StaticClass/Utility.cs
@@ -8,6 +8,9 @@ namespace Dun9eonAndFi9ht.StaticClass
 {
     public static class Utility
     {
+        const int startLine = 15;
+        static int currentLine = 15;
+
         /// <summary>
         /// 입력값의 범위를 받고 입력을 실행. 유효 입력 값인지 검사 후 결과 반환
         /// </summary>
@@ -16,20 +19,10 @@ namespace Dun9eonAndFi9ht.StaticClass
         /// <returns>에러코드(음수), 정상 입력값</returns>
         public static int UserInput(int min, int max)
         {
-            string firstInput = Console.ReadLine();
-            if (String.IsNullOrEmpty(firstInput))
-            {
-                return -1;       //빈 입력
-            }
-            int input;
-            if (!int.TryParse(firstInput, out input))
-            {
-                return -2;      //숫자가 아님
-            }
-            if (input > max || input < min)
-            {
-                return -3;      //범위 내 숫자가 아님
-            }
+            string? firstInput = Console.ReadLine();
+            if (String.IsNullOrEmpty(firstInput)) return -1;       //빈 입력
+            if (!int.TryParse(firstInput, out int input)) return -2;      //숫자가 아님
+            if (input > max || input < min) return -3;      //범위 내 숫자가 아님
             return input;
         }
 
@@ -39,8 +32,12 @@ namespace Dun9eonAndFi9ht.StaticClass
         /// <param name="message">출력 할 메시지</param>
         public static void PrintMenu(string message)
         {
-            Console.SetCursorPosition(0, 15);
+            if (currentLine == startLine)
+            {
+                PrintLine();
+            }
             Console.WriteLine(message);
+            UpdateCurrentLine();
         }
 
         /// <summary>
@@ -49,11 +46,48 @@ namespace Dun9eonAndFi9ht.StaticClass
         /// <param name="messages">출력할 메시지 string[]</param>
         public static void PrintMenu(string[] messages)
         {
-            Console.SetCursorPosition(0, 15);
+            if (currentLine == startLine)
+            {
+                PrintLine();
+            }
             foreach (string message in messages)
             {
                 Console.WriteLine(message);
             }
+            UpdateCurrentLine();
+        }
+
+        /// <summary>
+        /// 화면 분할("=====") 출력
+        /// </summary>
+        public static void PrintLine()
+        {
+            int width = Console.WindowWidth;
+            string line = new string('=', width);
+            Console.SetCursorPosition(0, startLine);
+            Console.WriteLine(line);
+            UpdateCurrentLine();
+        }
+
+        /// <summary>
+        /// 커서 위치 업데이트
+        /// </summary>
+        private static void UpdateCurrentLine()
+        {
+            currentLine = Console.CursorTop;
+        }
+
+        /// <summary>
+        /// 화면 분할 아래쪽을 지워버리는 역할
+        /// </summary>
+        public static void ClearMenu()
+        {
+            for (int i = startLine + 1; i < currentLine; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write(new String(' ', Console.WindowWidth));
+            }
+            currentLine = 15;
         }
     }
 }
