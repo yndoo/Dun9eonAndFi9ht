fc27231846c3960f22a7adc7240b80bb5d9d0a34
dalsi <tkdwns8632@gmail.com>
2025-02-07 10:39:11 +0900

    [ Feat ] DisplayStatus() 기능 구현, EJobType에 Warrior 추가
    // DisplayStatus() 기능 구현
    1. EJobType 값에 따라 맞는 이름 반환하기 위한 GetJobName(EJobType job) 추가
    
    // EJobType에 Warrior 추가

--- Dun9eonAndFi9ht/Characters/Player.cs
+++ Dun9eonAndFi9ht/Characters/Player.cs
@@ -8,7 +8,8 @@ namespace Dun9eonAndFi9ht.Characters
 {
     enum EJobType
     {
-        None
+        None,
+        Warrior
     }
     internal class Player : Character
     {
@@ -22,9 +23,38 @@ namespace Dun9eonAndFi9ht.Characters
         public int MaxExp { get; set; }
         public int CurExp { get; set; }
 
+        /// <summary>
+        /// 플레이어 스탯 살펴보기
+        /// </summary>
         public void DisplayStatus()
         {
-
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("상태 보기");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("캐릭터의 정보가 표시됩니다.");
+            Console.WriteLine();
+            Console.WriteLine($"Lv. {Level:D2}");
+            Console.WriteLine($"{Name} ( {GetJobName(Job)} )");
+            Console.WriteLine($"{"공격력"} : {Atk}");
+            Console.WriteLine($"{"방어력"} : {Def}");
+            Console.WriteLine($"{"체  력"} : {CurrentHp}");
+            Console.WriteLine($"{"Gold"}   : {gold}");
+        }
+        /// <summary>
+        /// EJobType에 따라 맞는 이름 반환
+        /// </summary>
+        /// <param name="job">EJobType 값</param>
+        /// <returns></returns>
+        private string GetJobName(EJobType job)
+        {
+            switch (job)
+            {
+                case EJobType.Warrior:
+                    return "전사";
+                default:
+                    return "";
+            }
         }
 
         public override void Attack(Character target)
