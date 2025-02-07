0ccc4d5fdb389981d918e2de0c1186c5a1c46fe0
N0name4-SDH <soh0927@naver.com>
2025-02-07 09:54:11 +0900

    [ Feat ] UserInput 및 PrintMenu 메서드 작성
    1. UserInput : 입력 값의 범위를 받고, 유효한 입력 값인지 검사 후 결과를 반환
       PrintMenu : string이나 string 배열을 입력 받고 그것을 콘솔 기준 y = 15에서 출력하게 만든다.

--- Dun9eonAndFi9ht/StaticClass/Utility.cs
+++ Dun9eonAndFi9ht/StaticClass/Utility.cs
@@ -6,16 +6,54 @@ using System.Threading.Tasks;
 
 namespace Dun9eonAndFi9ht.StaticClass
 {
-    static class Utility
+    public static class Utility
     {
+        /// <summary>
+        /// 입력값의 범위를 받고 입력을 실행. 유효 입력 값인지 검사 후 결과 반환
+        /// </summary>
+        /// <param name="min">입력 최소값</param>
+        /// <param name="max">입력 최대값</param>
+        /// <returns>에러코드(음수), 정상 입력값</returns>
         public static int UserInput(int min, int max)
         {
-            return 0;
+            string firstInput = Console.ReadLine();
+            if (String.IsNullOrEmpty(firstInput))
+            {
+                return -1;       //빈 입력
+            }
+            int input;
+            if (!int.TryParse(firstInput, out input))
+            {
+                return -2;      //숫자가 아님
+            }
+            if (input > max || input < min)
+            {
+                return -3;      //범위 내 숫자가 아님
+            }
+            return input;
         }
 
-        public static void PrintMenu(string str)
+        /// <summary>
+        /// 출력 시 일정 위치 아래에서 출력하게 만드는 코드 (0,15)
+        /// </summary>
+        /// <param name="message">출력 할 메시지</param>
+        public static void PrintMenu(string message)
         {
+            Console.SetCursorPosition(0, 15);
+            Console.WriteLine(message);
+        }
 
+        /// <summary>
+        /// 여러 string 출력 시 사용할 코드 (0,15) 위치에서 계속해서 출력
+        /// </summary>
+        /// <param name="messages">출력할 메시지 string[]</param>
+        public static void PrintMenu(string[] messages)
+        {
+            Console.SetCursorPosition(0, 15);
+            foreach (string message in messages)
+            {
+                Console.WriteLine(message);
+            }
         }
     }
 }
