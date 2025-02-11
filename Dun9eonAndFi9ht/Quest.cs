11374aa96c3ad589c56d1d053bba3562abd7a364
N0name4-SDH <soh0927@naver.com>
2025-02-11 17:05:08 +0900

    [ Fix ] 필드 데이터 타입 변화
    기존 구조에 사용하기 쉽도록 필드의 데이터 타입을 변화하였다.
    
    추가로 Utility에 object를 string[]으로 변환하는 메소드를 추가하였다.

--- Dun9eonAndFi9ht/Quest.cs
+++ Dun9eonAndFi9ht/Quest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dun9eonAndFi9ht.StaticClass;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,11 @@ namespace Dun9eonAndFi9ht
     public class Quest
     {
         public string QuestTitle { get; set; } // 퀘스트 제목
-        public string QuestDescription { get; set; } // 퀘스트 내용 설명
+        public string[] QuestDescription { get; set; } // 퀘스트 내용 설명
         public string TargetMonster { get; set; } // 목표 몬스터 이름
         public int NeedKills { get; set; } // 잡아야 할 몬스터 수
         public int CurrentKills { get; set; } // 현재 잡은 몬스터 수
-        public string RewardItem { get; set; } // 보상 아이템
+        public int RewardItem { get; set; } // 보상 아이템
         public int RewardMoney { get; set; } // 보상 금액
         public bool HasAccepted; //퀘스트를 수주한 상태인지 판단
         public bool IsCleared; //퀘스트 클리어 여부 판단
@@ -21,7 +22,7 @@ namespace Dun9eonAndFi9ht
 
 
         //퀘스트 생성자
-        public Quest(string questtitle, string questdescription, string targetMonster, int needKills, string rewardItem, int rewardMoney)
+        public Quest(string questtitle, string[] questdescription, string targetMonster, int needKills, int rewardItem, int rewardMoney)
         {
             QuestTitle = questtitle;
             QuestDescription = questdescription;
@@ -37,16 +38,35 @@ namespace Dun9eonAndFi9ht
         /// </summary>
         public virtual void ShowQuestInfo()
         {
-            Console.WriteLine($"퀘스트 제목: {QuestTitle}");
-            Console.WriteLine();
-            Console.WriteLine($"내용: {QuestDescription}");
-            Console.WriteLine();
-            Console.WriteLine($"{TargetMonster} {NeedKills}마리 처치 ({CurrentKills} / {NeedKills})");
-            Console.WriteLine();
-            Console.WriteLine("-보상-");
-            Console.WriteLine($"{RewardItem}");
-            Console.WriteLine($"{RewardMoney}G");
-            Console.WriteLine();
+            string[] desc = QuestDescription;
+            Utility.PrintScene($"{QuestTitle}");
+            Utility.PrintScene("");
+            Utility.PrintScene(desc);
+            Utility.PrintScene("");
+
+            Utility.PrintSceneW($"- {TargetMonster}");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Utility.PrintSceneW($" {NeedKills}");
+            Console.ResetColor();
+
+            Utility.PrintSceneW("마리 처치 (");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Utility.PrintSceneW($"{ CurrentKills}");
+            Console.ResetColor();
+
+            Utility.PrintSceneW("/");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Utility.PrintScene($"{ NeedKills})");
+            Console.ResetColor();
+
+            Utility.PrintScene("");
+            Utility.PrintScene("-보상-");
+            Utility.PrintScene($"  {RewardItem}");
+            Utility.PrintScene($"  {RewardMoney}G");
+            Utility.PrintScene("");
             ShowSelect();
         }
 
@@ -55,20 +75,40 @@ namespace Dun9eonAndFi9ht
         /// </summary>
         public void ShowSelect()
         {
+            Utility.ClearMenu();
             if (HasAccepted == false)
             {
-                Console.WriteLine("1. 수락");
-                Console.WriteLine("2. 거절");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Utility.PrintMenuW("1");
+                Console.ResetColor();
+                Utility.PrintMenu(". 수락");
+
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Utility.PrintMenuW("2");
+                Console.ResetColor();
+                Utility.PrintMenu(". 거절");
             }
             else if (CurrentKills < NeedKills)
             {
-                Console.WriteLine("클리어 조건이 충족되지 않았습니다.");
-                Console.WriteLine("0. 돌아가기");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Utility.PrintMenu("클리어 조건이 충족되지 않았습니다.");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Utility.PrintMenuW("0");
+                Console.ResetColor();
+                Utility.PrintMenu(". 돌아가기");
             }
             else
             {
-                Console.WriteLine("1. 보상 받기");
-                Console.WriteLine("2. 돌아가기");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("1");
+                Console.ResetColor();
+                Utility.PrintMenu(". 보상 받기");
+
+
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("2");
+                Console.ResetColor();
+                Utility.PrintMenu(". 돌아가기");
             }
         }
 
