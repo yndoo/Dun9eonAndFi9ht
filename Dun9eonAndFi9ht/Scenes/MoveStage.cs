e85b34b826f38df85cda04f02274810f9053c2f8
N0name4-SDH <soh0927@naver.com>
2025-02-11 13:39:47 +0900

    [ Chore ] Movestage 출력 조정
    출력 메시지를 숫자가 아닐 때, 범위 안에 없을 때, 갈 수 없는 층일 때로 세분화하여 출력

--- Dun9eonAndFi9ht/Scenes/MoveStage.cs
+++ Dun9eonAndFi9ht/Scenes/MoveStage.cs
@@ -15,14 +15,14 @@ namespace Dun9eonAndFi9ht.Scenes
             int stage = Dungeon.stage;
             int maxStageCleared = Dungeon.maxStageCleared;
             int nextinput = -1;
-            while (nextinput < 0)
+            while (true)
             {
                 Utility.ClearAll();
                 Utility.PrintScene($"현재 위치한 층을 이동합니다. (현재 {stage}층)");
                 Utility.PrintScene($"최고층 : {maxStageCleared + 1}층");
                 Utility.PrintMenu("층을 입력해 주세요 (0. 나가기)");
                 Utility.PrintMenu(">> ");
-                nextinput = Utility.UserInput(0, maxStageCleared + 1);
+                nextinput = Utility.UserInput(0, 5);
                 if (nextinput == 0) // 유저가 취소를 선택한 경우
                 {
                     Utility.PrintMenu("이전 메뉴로 돌아갑니다.");
@@ -42,6 +42,7 @@ namespace Dun9eonAndFi9ht.Scenes
                     Utility.PrintMenu("클리어하지 못한 층은 갈 수 없습니다.");
                     Utility.PrintMenu("아무 키나 입력\n>>");
                     Console.ReadLine();
+                    
                 }
                 else if(nextinput == stage)
                 {
@@ -51,14 +52,17 @@ namespace Dun9eonAndFi9ht.Scenes
                     Console.ReadLine();
                     nextinput = -1; // 루프를 계속 돌도록 초기화
                 }
+                else
+                {
+                    Utility.PrintScene($"{nextinput}층으로 이동합니다.");
+                    stage = nextinput;
+                    Utility.ClearMenu();
+                    Utility.PrintMenu("아무 키나 입력하세요");
+                    Console.Write(">> ");
+                    Console.ReadLine();
+                    return ESceneType.StartScene;
+                }
             }
-            Utility.PrintScene($"{nextinput}층으로 이동합니다.");
-            stage = nextinput;
-            Utility.ClearMenu();
-            Utility.PrintMenu("아무 키나 입력하세요");
-            Console.Write(">> ");
-            Console.ReadLine();
-            return ESceneType.StartScene;
         }
     }
 }
