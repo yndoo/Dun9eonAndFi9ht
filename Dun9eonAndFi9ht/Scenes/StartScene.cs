2043f0c1d1427c3bbf9446dd770687da85393206
Hwang-Inseop <inseop.h02@gmail.com>
2025-02-11 12:00:28 +0900

    [ Style ] 스타트씬 숫자 색상 변경
    선택지 숫자 색상 마젠타 색상으로 변경

--- Dun9eonAndFi9ht/Scenes/StartScene.cs
+++ Dun9eonAndFi9ht/Scenes/StartScene.cs
@@ -30,8 +30,18 @@ namespace Dun9eonAndFi9ht.Scenes
             while(true)
             {
                 Utility.ClearMenu();
-                Utility.PrintMenu(new string[] { "1. 상태보기", $"2. 전투 시작 (현재 : {Dungeon.stage}층)", "\n원하시는 행동을 입력해주세요.\n>>" });
-                int userInput = Utility.UserInput(1, 2);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Utility.PrintMenuW("1. ");
+                Console.ResetColor();
+                Utility.PrintMenu("상태보기");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Utility.PrintMenuW("2. ");
+                Console.ResetColor();
+                Utility.PrintMenu("전투 시작");
+                Utility.PrintMenuW("현재 ");
+                Utility.PrintMenuW($"{Dungeon.stage}");
+                Utility.PrintMenu("층\n\n원하시는 행동을 입력해주세요.\n>>");
+            int userInput = Utility.UserInput(1, 2);
                 if (userInput == 1)
                 {
                     return ESceneType.PlayerStat;
