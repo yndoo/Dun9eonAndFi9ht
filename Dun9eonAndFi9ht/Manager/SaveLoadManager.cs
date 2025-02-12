2954365b1fd31e10597f924a2188aa795cf4fcce
yndoo <isabelle2k@naver.com>
2025-02-12 10:17:08 +0900

    [ Chore ] SaveLoadManager 구조 조정

---
+++ Dun9eonAndFi9ht/Manager/SaveLoadManager.cs
@@ -0,0 +1,31 @@
+﻿using Dun9eonAndFi9ht.Characters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dun9eonAndFi9ht.Manager
+{
+    internal class SaveLoadManager
+    {
+        public void PlayerSave()
+        {
+
+        }
+        public void InventorySave()
+        {
+
+        }
+        public void DungeonSave()
+        {
+
+        }
+
+        public void GameSave()
+        {
+
+        }
+    }
+}
